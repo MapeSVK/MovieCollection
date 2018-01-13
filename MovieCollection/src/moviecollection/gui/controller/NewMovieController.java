e447cb0d86806e871d97da8d699a27812f5cc824
MapeSVK <michal.moravik@icloud.com>
2018-01-13 16:49:08 +0100

    "none" added into the comboBox

--- MovieCollection/src/moviecollection/gui/controller/NewMovieController.java
+++ MovieCollection/src/moviecollection/gui/controller/NewMovieController.java
@@ -7,11 +7,15 @@ package moviecollection.gui.controller;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
@@ -62,6 +66,7 @@ public class NewMovieController implements Initializable {
         this.model=model;
         this.selectedMovie=selectedMovie;
         firstCat.setItems(model.getAllCategories());
+        firstCat.getItems().add(new Category(-1, "none"));
         secCat.setItems(model.getAllCategories());
         thirdCat.setItems(model.getAllCategories());
         fill();
@@ -85,15 +90,30 @@ public class NewMovieController implements Initializable {
     
     @FXML
     private void SaveButtonClick(ActionEvent event) {
+        if (firstCat.getValue() != secCat.getValue() && 
+                secCat.getValue() != thirdCat.getValue() &&
+                thirdCat.getValue() != firstCat.getValue()) {
+                Save();
+                
+            
+        }
+        else {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("CATEGORY PROBLEM");
+            alert.setContentText("You cannot add movie to two same categories");
+            alert.showAndWait();
+        }
+    }
+    
+    private void Save(){
         if(selectedMovie==null)
         {
-        model.addMovie(new Movie(-1,
-                TitleTextField.getText(),
-                Double.valueOf(PRatingTextField.getText()),
-                Double.valueOf(IMDBRatingTextField.getText()),
-                FileTextField.getText(),
-                null));
-               
+            model.addMovie(new Movie(-1,
+            TitleTextField.getText(),
+            Double.valueOf(PRatingTextField.getText()),
+            Double.valueOf(IMDBRatingTextField.getText()),
+            FileTextField.getText(),
+            null));       
         }
         else if(selectedMovie!=null)
         {
@@ -103,7 +123,7 @@ public class NewMovieController implements Initializable {
             selectedMovie.setFilelink(FileTextField.getText());
             model.editMovies(selectedMovie);
         }
-    closeWindow();
+        closeWindow();
     }
  
     /*********** OTHER METHODS *************/
